69627a13d39b85eaa05985373ecf5673b1bc279e
Robert Santos <robertsantosti@outlook.com>
2023-08-29 21:08:26 -0300

    Criando casos de testes (com parametros) [red]

--- src/components/usuario/UsuarioComponent.test.jsx
+++ src/components/usuario/UsuarioComponent.test.jsx
@@ -20,3 +20,26 @@ describe('Suite de testes do usuário sem os dados passados por parametro', () =
     expect(screen.getByText(/E-mail do usuário não informado/i)).toBeInTheDocument();
   })
 })
+
+describe('Suite de testes do usuário com os dados passados por paramentro', () => {
+  const mock = {
+    nome: 'Robert Santos',
+    email: 'robert@teste.com.br'
+  }
+
+  beforeEach(() => {
+    render(<UsuarioComponent/>)
+  })
+
+  it('Verificando se o componente esta sendo exibido em tela', () => {
+    expect(screen.getByTestId('usuario-testid')).toBeInTheDocument();
+  })
+
+  it('Verificando se o nome do usuario esta sendo exibido', () => {
+    expect(screen.getByText(/Robert Santos/i)).toBeInTheDocument();
+  })
+  
+  it('Verificando se o email do usuario esta sendo exibido', () => {
+    expect(screen.getByText(/robert@teste.com.br/i)).toBeInTheDocument();
+  })
+})
