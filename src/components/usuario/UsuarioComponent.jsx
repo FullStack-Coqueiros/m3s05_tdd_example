b32b7404f97f09fecaf2d794b40afa2845e87064
Robert Santos <robertsantosti@outlook.com>
2023-08-29 21:01:53 -0300

    Casos de teste (sem parametro) [green]

---
+++ src/components/usuario/UsuarioComponent.jsx
@@ -0,0 +1,9 @@
+
+export const UsuarioComponent = () => {
+  return (
+    <div data-testid="usuario-testid">
+      <p>Nome do usuário não informado</p>
+      <p>E-mail do usuário não informado</p>
+    </div>
+  )
+}
--- src/components/usuario/UsuarioComponent.test.jsx
+++ src/components/usuario/UsuarioComponent.test.jsx
@@ -1,8 +1,13 @@
 import '@testing-library/jest-dom';
 
-import { screen } from '@testing-library/dom';
+import { screen, render } from '@testing-library/react';
+import { UsuarioComponent } from './UsuarioComponent';
 
 describe('Suite de testes do usuário sem os dados passados por parametro', () => {
+  beforeEach(() => {
+    render(<UsuarioComponent/>)
+  })
+
   it('Verificando se o componente esta sendo exibido em tela', () => {
     expect(screen.getByTestId('usuario-testid')).toBeInTheDocument();
   })
@@ -10,7 +15,7 @@ describe('Suite de testes do usuário sem os dados passados por parametro', () =
   it('Verificando se o nome do usuario esta sendo exibido com a tratativa', () => {
     expect(screen.getByText(/Nome do usuário não informado/i)).toBeInTheDocument();
   })
-
+  
   it('Verificando se o email do usuario esta sendo exibido com a tratativa', () => {
     expect(screen.getByText(/E-mail do usuário não informado/i)).toBeInTheDocument();
   })
