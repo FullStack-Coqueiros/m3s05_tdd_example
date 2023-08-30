c8eec62eded0d25fbc87079ce14ab48c0b63ad21
Robert Santos <robertsantosti@outlook.com>
2023-08-29 21:04:19 -0300

    Criando casos de testes (sem params) [refactor]

--- src/components/usuario/UsuarioComponent.jsx
+++ src/components/usuario/UsuarioComponent.jsx
@@ -1,9 +1,9 @@
 
-export const UsuarioComponent = () => {
+export const UsuarioComponent = ({nome, email}) => {
   return (
     <div data-testid="usuario-testid">
-      <p>Nome do usuário não informado</p>
-      <p>E-mail do usuário não informado</p>
+      <p>{ nome || 'Nome do usuário não informado' }</p>
+      <p>{ email || 'E-mail do usuário não informado'}</p>
     </div>
   )
 }
